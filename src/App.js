592175c55e52abfb23aaef69d607da4d63c48287
jremy813 <jremywatkins@gmail.com>
2020-09-26 19:03:05 -0400

    created slider component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Slider />
     </div>
   );
 }
