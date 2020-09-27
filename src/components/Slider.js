3e0720ddc9797c24b2a950d9391c5ee6a0ae6be8
jremy813 <jremywatkins@gmail.com>
2020-09-27 13:15:16 -0400

    finished styling slider images

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Slider.scss";
+import ShoeImg from "../assets/Active-image3.png";
+import Background1 from "../assets/Active-image.png";
+import Background2 from "../assets/Active-image2.png";
+import PriceTag from "../assets/Price tag.png";
 
 function Slider() {
-  return <div className="slider"></div>;
+  return (
+    <div className="slider">
+      <img className="slider__bg1 slider__img" src={Background1} alt="" />
+      <img className="slider__shoe slider__img" src={ShoeImg} alt="shoe" />
+      <img className="slider__bg2 slider__img" src={Background2} alt="" />
+      <img className="slider__tag slider__img" src={PriceTag} alt="price" />
+    </div>
+  );
 }
 
 export default Slider;
